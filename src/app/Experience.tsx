6707022d2d166a56701c4ac915c50c9bc0b823d9
josephdib3160 <134255797+josephdib3160@users.noreply.github.com>
2025-12-09 13:12:00 -0800

    added experience

--- src/app/Experience.tsx
+++ src/app/Experience.tsx
@@ -37,9 +37,23 @@ export default function Experience() {
       </header>
       {/* About Section */}
       <section id="about" className="mx-auto p-8">
-        <h2 className="text-2xl font-semibold mb-4 border-b-2 border-gray-400 pb-2">About Me</h2>
         <p>
-          Hi! Experience will go here.
+          Below you will find more information about my work experience.
+        </p>
+      </section>
+
+      <section id="about" className="mx-auto p-8">
+        <h2 className="text-xl font-semibold mb-4 border-gray-400 ">Pushabl, LLC</h2>
+        <h2 className="text-xl font-semibold mb-4 border-gray-400 ">July 2025 - November 2025</h2>
+        <h2 className="text-xl font-semibold mb-4 border-b-2 pb-2 border-gray-400 ">Software Engineer Coordinator</h2>
+        <p>
+          This was my first real experience in the field. I originally started out as an intern, then was given an offer to work full-time after 2 months. 
+          The company size was small, with there only being 3 other developers working on multiple different projects.
+        </p>
+
+        <p>
+        The main project that I worked on, however, was an app called Flyertap. This app was built using React Native. 
+        I primarily worked on the front-end part of the app, but would also make backend and database additions or changes if needed.
         </p>
       </section>
 
